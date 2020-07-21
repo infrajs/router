d629563c334a2bd6c2651b5d892573deabdf2593
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-07-21 12:02:33 +0400

    fix

--- 404.php
+++ 404.php
@@ -1,7 +1,7 @@
 <?php
 //404 ошибка
 $query = $_SERVER['REQUEST_URI'];
-echo '<h1>'.$query.'</h1>';
-echo '404 Not Found';
 header("Status: 404 Not Found");
 http_response_code(404);
+echo '<h1>'.$query.'</h1>';
+echo '404 Not Found';
